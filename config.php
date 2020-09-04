116f276cd446f4c7d9c7352e8e79a936fa3a6315
mohammedmrm <60153318+mohammedmrm@users.noreply.github.com>
2020-09-04 16:15:18 +0300

    update

--- config.php
+++ config.php
@@ -9,5 +9,11 @@ $config = [
    "theme-config"=> 'data-header="light" data-footer="dark" data-header_align="app" data-menu_type="center" data-menu="light" data-menu_icons="on" data-footer_type="left" data-site_mode="light" data-footer_menu="show" data-footer_menu_style="dark"',
    "dev_b"=>5000,               //??? ??????? ?????
    "dev_o"=>10000                //??? ??????? ????? ?????????
-]
+];
+require_once("php/dbconnection.php");
+$sql = "select * from setting";
+$setting = getData($con,$sql);
+foreach($setting as $val){
+  $config[$val['control']] =  $val['value'];
+}
 ?>
